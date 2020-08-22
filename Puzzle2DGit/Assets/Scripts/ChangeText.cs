9fc44c7c48af680d5023357044262bd2916ddc1d
SMHeyne <u35447@hs-harz.de>
2020-08-22 09:20:21 +0200

    buttons text change on hover

---
+++ Puzzle2DGit/Assets/Scripts/ChangeText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text Hov;
+    public Text NoHov;
+
+    void Start()
+    {
+        Hov.gameObject.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Hov.gameObject.SetActive(true);
+        NoHov.gameObject.SetActive(false);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Hov.gameObject.SetActive(false);
+        NoHov.gameObject.SetActive(true);
+    }
+}
