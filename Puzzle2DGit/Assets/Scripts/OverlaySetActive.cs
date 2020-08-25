63cf957443a9a52bdda4745c90d21e4318e62c8a
SMHeyne <u35447@hs-harz.de>
2020-08-25 18:02:49 +0200

    added scripts open overlay + changed main menu

---
+++ Puzzle2DGit/Assets/Scripts/OverlaySetActive.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OverlaySetActive : MonoBehaviour
+{
+    public GameObject Overlay;
+
+    // Start is called before the first frame update
+    public void OpenOverlay()
+    {
+        if(Overlay != null)
+        {
+            bool isActive = Overlay.activeSelf;
+
+            Overlay.SetActive(!isActive);
+        }
+    }
+}
