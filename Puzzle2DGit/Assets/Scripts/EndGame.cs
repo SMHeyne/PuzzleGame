51bcc572d24e29625b5712f0747ca76a928bd702
inKospor <35812538+inKospor@users.noreply.github.com>
2020-08-26 10:01:31 +0200

    MainMenu
    MM works, new Buttons für 4 and 5

--- Puzzle2DGit/Assets/Scripts/EndGame.cs
+++ Puzzle2DGit/Assets/Scripts/EndGame.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class EndGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Quit()
     {
-        
-    }
+        Application.Quit();
+     
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
