58440564865104883b8bd97236a316dd38a0062b
inKospor <35812538+inKospor@users.noreply.github.com>
2020-08-27 17:10:01 +0200

    Finale
    Final changes

--- Puzzle2DGit/Assets/Scripts/Loader.cs
+++ Puzzle2DGit/Assets/Scripts/Loader.cs
@@ -5,11 +5,46 @@ using UnityEngine;
 
 public class Loader : MonoBehaviour
 {
-
+    public GameObject Overlay;
 
     public void Load  (int i)
     {
         SceneManager.LoadScene(i);
+    }
+    private void Update()
+    {
+
+        if (Overlay != null && Input.GetKeyDown(KeyCode.Escape))
+        {
+            bool isActive = Overlay.activeSelf;
+
+            Overlay.SetActive(!isActive);
+        }
+
+
+        /**
+       if (Input.GetKey(KeyCode.Escape))
+       {
+           GameObject escOV = GetComponent < OverlaySetActive>().Overlay;
+           bool isActive = escOV.activeSelf;
+
+           escOV.SetActive(!isActive);
+       }
+       */
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void OpenOverlay()
+    {
+        if (Overlay != null)
+        {
+            bool isActive = Overlay.activeSelf;
+
+            Overlay.SetActive(!isActive);
+        }
     }
 
 }
